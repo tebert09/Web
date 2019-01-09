0d56d689e9a87c9d638d5d1f84d7c910eecf44cf
eber0222 <eber0222@umn.edu>
2019-01-08 19:16:01 -0600

    home page and navigation updates; mobile design

--- js/terri-ebert.js
+++ js/terri-ebert.js
@@ -3,15 +3,15 @@
 // ==================================================
 
 $(document).ready(function () {
-    $("#skills .skill").hide(); // Initially hide all tab content
+    $("#tabs .skill").hide(); // Initially hide all tab content
     $("#skill-selector li:first").attr("id", "active"); // Activate first li
-    $("#skills .skill:first").fadeIn(); // Show first tab content
+    $("#tabs .skill:first").fadeIn(); // Show first tab content
     $('#skill-selector li a').click(function (e) {
         e.preventDefault();
         if ($(this).attr("id") == "active") { // Detect the tab with active class
             return
         } else {
-            $("#skills .skill").hide(); //Hide all tab content
+            $("#tabs .skill").hide(); //Hide all tab content
             $("#skill-selector li").attr("id", ""); // Reset id's on li 
             $(this).parent().attr("id", "active"); // Activate the active li
             $($(this).attr('href')).fadeIn(); // Show content for current tab
